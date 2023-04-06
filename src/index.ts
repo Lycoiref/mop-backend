77eba8404146d42be774a40f24769dc127ffde64
Lycoiref <1808347356@qq.com>
2023-04-06 20:37:37 +0800

    在初次使用时init admin用户

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import Koa from 'koa'
 import Router from 'koa-router'
 import cors from 'koa2-cors'
-import api from './api/api'
-import user from './api/user'
+import { initAdmin } from './utils/database/index'
+import { api, user } from './api/index'
 
 const app = new Koa()
 const router = new Router()
@@ -17,6 +17,7 @@ router.use('/user', user.routes(), user.allowedMethods())
 // cors跨域
 app.use(cors())
 app.use(router.routes())
-app.listen(3000, () => {
+app.listen(3000, async () => {
+    await initAdmin()
     console.log('server is running at http://localhost:3000')
 })
--- src/utils/database/index.ts
+++ src/utils/database/index.ts
@@ -1,2 +1,22 @@
 import prisma from "./database"
 
+export async function initAdmin() {
+    const admin = await prisma.users.findFirst({
+        where: {
+            username: "admin"
+        }
+    })
+
+    if (!admin) {
+        console.log("admin not found, creating admin")
+        let result = await prisma.users.create({
+            data: {
+                username: "admin",
+                password: "admin",
+                role: "ADMIN"
+            }
+        })
+        console.log("admin created", result)
+    }
+    return admin
+}
